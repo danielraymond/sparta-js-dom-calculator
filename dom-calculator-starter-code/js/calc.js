11316744be7f183db45ffa62bf387ee191eda09f
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-11-01 21:23:50 +0000

    added dom code to get value of a button

---
+++ dom-calculator-starter-code/js/calc.js
@@ -0,0 +1,22 @@
+
+  // user input storage
+  function userInputStorage(numberOne, numberTwo, operator) {
+
+  }
+
+  // var test = userInputStorage();
+  // console.log(test);
+
+
+
+
+  // create dom events for each button
+
+  var number = document.getElementsByTagName('button')[1].textContent;
+  console.log(number);
+
+  // have a storage for which numbers are used
+
+  // have a storage for each operator used
+
+  // create functions for what each operator does
