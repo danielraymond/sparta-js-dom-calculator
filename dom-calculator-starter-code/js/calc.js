87c3d601a91b7b1f13d3a1c58be65edc688d2ad2
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-11-01 21:59:33 +0000

    edited function to check button input

--- dom-calculator-starter-code/js/calc.js
+++ dom-calculator-starter-code/js/calc.js
@@ -15,14 +15,30 @@
   // var number = document.getElementsByTagName('button')[1].textContent;
   // console.log(number);
 
-  var buttons = document.querySelectorAll('.buttonNum');
+var buttons = document.querySelectorAll('.buttonNum');
 
-  for (var i = 0; i < buttons.length; i++) {
-    var button = buttons[i];
-    button.addEventListener("click", function(event){
-      console.log(this.innerHTML + " was clicked");
-    })
+// for (var i = 0; i < buttons.length; i++) {
+//   button.addEventListener("click", buttonType)
+//
+//   function buttonType() {
+//     console.log(this.innerHTML + " was clicked");
+//     return this.innerHTML;
+//   }
+//   test = buttontype();
+//   console.log(test);
+// }
+
+for (var i = 0; i < buttons.length; i++) {
+  var button = buttons[i];
+  button.addEventListener("click", buttonType);
+
+  function buttonType() {
+    console.log(this.innerHTML + " was clicked");
+    var input = this.innerHTML;
+    return input;
   }
+}
+
 
   // have a storage for which numbers are used
 
