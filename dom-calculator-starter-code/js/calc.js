f3deb6d6dc7d5d933786870bd929e7c2af8bed6a
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-11-01 21:35:53 +0000

    added function to return the value of the number when clicked

--- dom-calculator-starter-code/js/calc.js
+++ dom-calculator-starter-code/js/calc.js
@@ -12,8 +12,17 @@
 
   // create dom events for each button
 
-  var number = document.getElementsByTagName('button')[1].textContent;
-  console.log(number);
+  // var number = document.getElementsByTagName('button')[1].textContent;
+  // console.log(number);
+
+  var buttons = document.querySelectorAll('.buttonNum');
+
+  for (var i = 0; i < buttons.length; i++) {
+    var button = buttons[i];
+    button.addEventListener("click", function(event){
+      console.log(this.innerHTML + " was clicked");
+    })
+  }
 
   // have a storage for which numbers are used
 
