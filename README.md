c570682b10aaa06e9e420848035fae51b4b506c4
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-11-05 13:16:13 +0000

    changed location of files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# sparta-js-dom-calculator
+### Brief
+
+I had trouble coming up with a basic method of how to do this as I found the DOM methods quite confusing. I knew how they worked and what they did but I struggled to put them into practice and understand how to use them in other parts of the code. This combined with being very tired meant I could not come up with a plan.
---
+++ js/calc.js
@@ -0,0 +1,26 @@
+  // user input storage
+  // function userInputStorage(numberOne, numberTwo, operator) {
+  }
+  // var test = userInputStorage();
+  // console.log(test);
+  // create dom events for each button
+
+  // var number = document.getElementsByTagName('button')[1].textContent;
+  // console.log(number);
+
+// var buttons = document.querySelectorAll('.buttonNum');
+//
+// for (var i = 0; i < buttons.length; i++) {
+//   var button = buttons[i];
+//   button.addEventListener("click", buttonType);
+//   buttonType();
+// }
+//
+// function buttonType() {
+//   console.log(this.innerHTML + " was clicked");
+//   var input = this.innerHTML;
+//   return input;
+// }
+  // have a storage for which numbers are used
+  // have a storage for each operator used
+  // create functions for what each operator does
